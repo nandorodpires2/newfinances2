5cd3008232e59df5e45ae3a0d7b6b1c21714b38d
nandorodpires2 <contato@newfinances.com.br>
2014-08-22 10:53:32 -0300

    inclusoes

---
+++ application/helpers/views/Cartoes.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Cartoes
+ *
+ * @author Fernando Rodrigues
+ */
+class View_Helper_Cartoes extends Zend_View_Helper_Abstract {
+    
+    public static function getBandeiraCartao($bandeira) {
+        
+        $view = Zend_Layout::getMvcInstance()->getView();
+        $img_path = $view->baseUrl() . '/views/img/cartoes/';
+        
+        switch ($bandeira) {
+            case 'visa':
+                $img_path .= "icon_visa.png";
+                break;
+            case 'mastercard':
+                $img_path .= "icon_mastercard.png";
+                break;
+            default:
+                return 'Nunhuma';
+                break;
+        }
+        
+        $return = "<img class='img img-thumbnail' src='{$img_path}' />";
+        
+        return $return;
+        
+    }
+    
+}
