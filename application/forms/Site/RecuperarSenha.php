5181f93305ee429d9c37a493e7476116deb0ccaf
nandorodpires2 <contato@newfinances.com.br>
2014-12-14 17:33:00 -0200

    atualizacoes

--- application/forms/Site/RecuperarSenha.php
+++ application/forms/Site/RecuperarSenha.php
@@ -22,6 +22,22 @@ class Form_Site_RecuperarSenha extends Zend_Form {
             'class' => 'form-control'
         ));
         
+        /*
+        $captchaImage = new Zend_Captcha_Image();
+        $captchaImage->setExpiration(300)
+                ->setWordlen(8)
+                ->setWidth(255)
+                ->setHeight(60)
+                ->setImgUrl("/public/views/images/captcha/")
+                ->setImgDir("./views/images/captcha/")
+                ->setFont(PUBLIC_PATH . "/views/images/captcha/DIN-Bold.otf");
+                
+        $this->addElement('captcha', 'captcha', array(
+            'class' => 'form-control',
+            'placeholder' => 'Digite os caracteres da imagem',
+            'captcha' => $captchaImage                         
+        ));
+        */
         //submit
         $this->addElement('submit', 'submit', array(
             'label' => 'Enviar',
