a81c3f084ec06e7a22f479ec33791b4a41d52d69
nandorodpires2 <contato@newfinances.com.br>
2015-03-04 22:27:05 -0300

    atualizacoes

---
+++ public/views/js/cliente/pendencias/index.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){    
+    $("#todos").click(function(){    
+        $('.pendencias').each(function() {            
+            var checked = $(".pendencias").prop("checked");                        
+            if (checked) {
+                $(".pendencias").prop("checked", false);
+            } else {
+                $(".pendencias").prop("checked", true);               
+            }
+        });
+    });
+});
+
