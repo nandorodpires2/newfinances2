f21b8e9dfd5b8cecba3032b18c085aef2edb7de6
nandorodpires2 <contato@newfinances.com.br>
2015-04-27 13:01:01 -0300

    atualizacoes

--- application/models/Log.php
+++ application/models/Log.php
@@ -16,4 +16,26 @@ class Model_Log extends Zend_Db_Table {
     protected $_name = "log";
     protected $_primary = "id_log";
     
+    public function getQueryAll() {
+        $select = $this->select()
+                ->from(array('l' => $this->_name), array('*'))
+                ->setIntegrityCheck(false)
+                ->joinLeft(array('u' => 'usuario'), 'l.id_usuario = u.id_usuario', array('*'));
+        
+        return $select;
+    }
+    
+    public function getLogById($id_log) {
+        $select = $this->getQueryAll()
+                ->where("id_log = ?", $id_log);
+        return $this->fetchRow($select);
+    }
+    
+    public function getLogs() {
+        $select = $this->getQueryAll()
+                ->order(array("data_log desc"))
+                ->limit(100);                
+        return $this->fetchAll($select);
+    }
+    
 }
